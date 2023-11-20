6757bbdf1be5f02ec764a81117fbd80a8078fd07
Antonio Marques <tonysenha@gmail.com>
2023-11-20 12:26:02 -0300

    att do chat
    atualização do codigo, algumas alterações no backend e no frontend, futuramente o ideal seria conectar a uma db

--- Chat/backend/index.js
+++ Chat/backend/index.js
@@ -1,26 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios = require("axios")
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
+const CHAT_ENGINE_PROJECT_ID = "e656fe09-939a-4b37-ad49-a5dcb119782f";
+const CHAT_ENGINE_PRIVATE_KEY = "1aec8e39-1eaf-41c7-b082-e16736408b08";
 
-  try{
-    const r = await axios.put('https://api.chatengine.io/users/',
-    {username: username, secret:  username, first_name: username},
-    {headers: {"private-key": "93260d96-a5a3-42bc-932d-6419a44a8ad0"}}
-    );
+app.post("/authenticate", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { 
+        username, secret, email, first_name, last_name
+      },
+      {
+        headers: {"private-key": CHAT_ENGINE_PRIVATE_KEY}
+      }
+  )
+  return res.status(r.status).json(r.data)
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data)
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
     return res.status(r.status).json(r.data);
   } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
-
-
-
-  return res.json({ username: username, secret: "sha256..." });
 });
+
 app.listen(3001);
--- Chat/frontend/src/ChatsPage.jsx
+++ Chat/frontend/src/ChatsPage.jsx
@@ -1,15 +1,18 @@
-import { PrettyChatWindow } from 'react-chat-engine-pretty'
-const ChatsPage = (props) => {
-    return( 
-    <div style={{height: '100vh'}}>
-        <PrettyChatWindow
-        projectId='15ef9dd4-a4ba-4622-9449-316b1f24dde2'
-        username={props.user.username}
-        secret={props.user.secret}
-        style={{height: '100%'}}
-        />
-    </div> 
-    )
-}
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-export default ChatsPage
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="e656fe09-939a-4b37-ad49-a5dcb119782f"
+        // eslint-disable-next-line react/prop-types
+        username={props.user.username}
+        // eslint-disable-next-line react/prop-types
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
